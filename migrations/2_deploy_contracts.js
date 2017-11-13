2f001630657537fa515efa806da4fae15732705f
Nick Geoca <nickgeoca@gmail.com>
2017-11-13 01:35:44 -0600

    Revised migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -21,40 +21,40 @@ const duration = {
 
 module.exports = function (deployer, chain, accounts) {
   return deployer.deploy(SafeMath).then(async () => {
-  // Parameters
-  const timeFromStart = duration.hours(5);                   // seconds
-  const presaleDuration = duration.seconds(60);              // seconds
-  const walletAddress = '0x7a4baa345548aa30f11ffa61d2a7a685ea4537a9';
-  const presaleSupplyCap = 100;                              // tokens
-  const minimumInvestment = 0.1;                             // ether
-  const blockDuration = 14;                                  // seconds
 
-  // Deployment
-    const lastBlock = 10 ;//await latestBlockNumber();
-  const startBlock = lastBlock + blockDuration * timeFromStart;
-  const endBlock = startBlock + blockDuration * presaleDuration;
-  const weiMinimumInvestment = new BigNumber(10**18) * new BigNumber(minimumInvestment);  // wei
-  console.log('Start block -' + startBlock);
-  console.log('End block -'   + endBlock);
-
-  await deployer.deploy(SafeMath)
-  await deployer.deploy(MiniMeTokenFactory);
-  await deployer.deploy(APT, MiniMeTokenFactory.address);
-  await deployer.deploy(PlaceHolder, APT.address);
-  deployer.link(SafeMath, PreSale);
-  await deployer.deploy(PreSale, APT.address, PlaceHolder.address);
-
-  const apt = await APT.deployed();
-  const ps = await PreSale.deployed();
-
-  await apt.changeController(PreSale.address)
-  await ps.initialize(
-    walletAddress,
-    presaleSupplyCap,
-    weiMinimumInvestment,
-    startBlock,
-    endBlock
-  ); 
-
-  })};
+    // Parameters
+    const timeFromStart = duration.hours(5);                   // seconds
+    const presaleDuration = duration.seconds(60);              // seconds
+    const walletAddress = '0x7a4baa345548aa30f11ffa61d2a7a685ea4537a9';
+    const presaleSupplyCap = 100;                              // tokens
+    const minimumInvestment = 0.1;                             // ether
+    const blockDuration = 14;                                  // seconds
+    
+    // Deployment
+    const lastBlock = await latestBlockNumber();
+    const startBlock = lastBlock + blockDuration * timeFromStart;
+    const endBlock = startBlock + blockDuration * presaleDuration;
+    const weiMinimumInvestment = new BigNumber(10**18) * new BigNumber(minimumInvestment);  // wei
+    console.log('Start block -' + startBlock);
+    console.log('End block -'   + endBlock);
+    
+    await deployer.deploy(MiniMeTokenFactory);
+    await deployer.deploy(APT, MiniMeTokenFactory.address);
+    await deployer.deploy(PlaceHolder, APT.address);
+    deployer.link(SafeMath, PreSale);
+    await deployer.deploy(PreSale, APT.address, PlaceHolder.address);
+    
+    const apt = await APT.deployed();
+    const ps = await PreSale.deployed();
+    
+    await apt.changeController(PreSale.address)
+    await ps.initialize(
+      walletAddress,
+      presaleSupplyCap,
+      weiMinimumInvestment,
+      startBlock,
+      endBlock
+    ); 
+  }
+)};
 
